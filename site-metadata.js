f49ca1759298ddc5caad7efca789f4cb302b08a0
Sean Young <seany@Seans-MacBook-Pro-15.local>
2025-11-05 22:41:04 -0500

    update

---
+++ site-metadata.js
@@ -0,0 +1,25 @@
+// site-metadata.js
+
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-K3B3NZKEEL');
+
+(function(){
+  var ga = document.createElement('script');
+  ga.async = true;
+  ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-K3B3NZKEEL';
+  document.head.appendChild(ga);
+})();
+
+// Tawk.to live chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function(){
+  var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/6679f68f9d7f358570d2ec6f/1i166m88j';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
